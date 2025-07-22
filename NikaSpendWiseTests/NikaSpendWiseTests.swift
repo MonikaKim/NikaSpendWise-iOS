26d2b9b44abe4e6c8358a700c4a13cd6fd5e94de
kim.monika <kim.monika@sathapana.com.kh>
2025-07-22 14:30:16 +0700

    Initial Commit

---
+++ NikaSpendWiseTests/NikaSpendWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  NikaSpendWiseTests.swift
+//  NikaSpendWiseTests
+//
+//  Created by Kim Monika on 22/7/25.
+//
+
+import Testing
+@testable import NikaSpendWise
+
+struct NikaSpendWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
